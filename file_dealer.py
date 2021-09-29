d3298960b31ece5e1a2536e306b2aca846a0a812
Clara Dantas <clara.moraesd@gmail.com>
2021-09-28 22:45:35 -0300

    Fix missing quotation.

--- file_dealer.py
+++ file_dealer.py
@@ -39,32 +39,38 @@ def merge_payments():
 # 3. merge two files (payments and details)
 def merge_details_and_payments():
     current_line = 1
-    with open('./transrobot/payments_details.csv', 'a') as new_file:
+    with open('./transrobot/payments_details2.csv', 'a') as new_file:
         line_header = 0
+        new_file_writer = csv.writer(new_file,
+                                     delimiter=',',
+                                     quotechar='"',
+                                     quoting=csv.QUOTE_MINIMAL)
+
         with open('./transrobot/payments_merged.csv', 'r') as payment_file:
-            for line in payment_file:
+            payment_file_csv = csv.reader(payment_file, delimiter=',', quotechar='"')
+            for line in payment_file_csv:
                 print(current_line)
                 current_line += 1
+
                 with open('./transrobot/details_merged.csv', 'r') as details_file:
+                    details_csv = csv.reader(details_file, delimiter=',', quotechar='"')
                     if line_header == 0:
-                        details_header = details_file.__next__()
-                        new_header = line.rstrip() + ',' + details_header
-                        new_file.write(new_header)
+                        details_header = details_csv.__next__()
+                        new_file_writer.writerow(line + details_header)
                         line_header += 1
 
                     else:
-                        payment_values = line.rstrip().split(',')
+                        # payment_values = line.rstrip().split(',')
 
                         # 1 = empenho, 2 = favorecido
-                        empenho = payment_values[1].split('NE')[-1]
+                        empenho = line[1].split('NE')[-1]
                         # Remove trailling 0
-                        payment_id = str(int(empenho)) + payment_values[2]
+                        payment_id = str(int(empenho)) + line[2]
                         value = ['NA' for i in range(22)]
-                        value = ','.join(value)
-                        value += '\n'
+                        # value = ','.join(value)
+                        # value += '\n'
                         details_line = 1
 
-                        details_csv = csv.reader(details_file, delimiter=',', quotechar='"')
                         for line_detail in details_csv:
                             if details_line == 1:
                                 details_line += 1
@@ -72,10 +78,10 @@ def merge_details_and_payments():
 
                             detail_id = str(int(line_detail[14])) + line_detail[13]
                             if payment_id == detail_id:
-                                    value = ','.join(line_detail)
+                                    value = line_detail
                                     break
-                            
 
-                        new_file.write(line.rstrip() + ',' + value + '\n')
+
+                        new_file_writer.writerow(line + value)
 
 merge_details_and_payments()
